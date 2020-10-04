1c8fbf77bf12ddc63c94b5055b1a74b96a0e7738
unknown <ygpramuditha@gmail.com>
2020-10-04 11:52:00 +0530

    menu.php index.php update | phpmailer

--- includes/menu.php
+++ includes/menu.php
@@ -1,5 +1,23 @@
-<?php
+<div class="topnav">
+    
+    <div id="nav-left">
+        <img src="img/logo.png" alt="logo" width="45%" height="40%">
+        </div>
 
-echo "This is menu php";
+    <div id="nav-center">  
+   <a class="active" href="#home">Home</a>
+   <a href="#aboutus">About Us</a>
+  <a href="#services">Services</a>
+  <a href="#gallery">Gallery</a>
+  <a href="#contact">Contact</a>
+   </div>
+ 
+  
 
-?>
+    <div id="nav-right">
+    <a href="#login">Login</a>
+    </div>
+
+</div>
+
+
--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php
 include 'includes/header.php';
+?>
+<link rel="stylesheet" href="css/home.css">
+<title>Home</title>
+</head>
+<body>
+
+<?php
+include 'includes/menu.php';
+?>
+
+
+<?php
 include 'includes/footer.php';
 ?>
+
+
+</body>
+</html>

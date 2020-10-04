25d949fb64ffb5df05aa0356a5a650f089643ba2
Mahagedara <kehanmahagedara@gmail.com>
2020-10-04 13:59:42 +0530

    Hiruja

---
+++ contact.php
@@ -0,0 +1,17 @@
+<?php
+include 'includes/header.php';
+?>
+
+<link rel="stylesheet" href="css/contact.css">
+<title>Contact</title>
+</head>
+<body>
+<?php
+include 'includes/menu.php';
+?>
+
+
+
+<?php
+include 'includes/footer.php';
+?>
---
+++ signup.php
@@ -0,0 +1,11 @@
+<?php
+include 'includes/header.php';
+?>
+
+<link rel="stylesheet" href="css/signup.css">
+<title>Signup</title>
+</head>
+<body>
+
+</body>
+</html>

5787f5c081f78455c59df86308ab503617e54da9
unknown <achinimalsha4@gmail.com>
2020-10-04 18:11:04 +0530

    about us achini

--- aboutus.php
+++ aboutus.php
@@ -9,7 +9,29 @@ include 'includes/header.php';
 <?php
 include 'includes/menu.php';
 ?>
-<!-- write the code here -->
+<div class="row">
+ <div class="column">
+ <img src="./img/about-image.jpg" alt="wedding" style="width:100% height:50%" />
+ </div>
+ <div class="column">
+ <h2>TWO RINGS</h2>
+<p>We, Two Rings, bring you the most elegant wedding needs/requirements for your wedding, engagement, reception and homecoming.</p>
+
+<h4>Wedding stationery:</h4>
+
+<p>We sell a wide range of wedding products including wedding favours, invitations and cake boxes.</p>
+
+<h4>Suppliers:</h4>
+
+<p>We select our suppliers very carefully to maintain the quality at a reasonable price.</p>
+
+<h4>Privacy:</h4>
+
+<p>We at The Wedding Shop , are highly concerned about your privacy. The information you provide us will be used solely for any future communication with you.
+
+* Your delivery address and phone number will be shared with the courier company.</p>
+</div>
+</div>
 
 
 

b59ad47659794f6f1037ed041c2d5b5967bf6c6b
unknown <achinimalsha4@gmail.com>
2020-10-04 19:43:13 +0530

    aboutus

--- aboutus.php
+++ aboutus.php
@@ -9,29 +9,59 @@ include 'includes/header.php';
 <?php
 include 'includes/menu.php';
 ?>
+<div class="banner">
+</div>
 <div class="row">
  <div class="column">
- <img src="./img/about-image.jpg" alt="wedding" style="width:100% height:50%" />
+ <h2>TWO RINGS</h2>
  </div>
  <div class="column">
- <h2>TWO RINGS</h2>
 <p>We, Two Rings, bring you the most elegant wedding needs/requirements for your wedding, engagement, reception and homecoming.</p>
-
 <h4>Wedding stationery:</h4>
-
 <p>We sell a wide range of wedding products including wedding favours, invitations and cake boxes.</p>
-
 <h4>Suppliers:</h4>
-
-<p>We select our suppliers very carefully to maintain the quality at a reasonable price.</p>
-
+<p>We select our suppliers very carefully to maintain the quality at a reasonable price.</p></div>
+<div class="column">
 <h4>Privacy:</h4>
-
-<p>We at The Wedding Shop , are highly concerned about your privacy. The information you provide us will be used solely for any future communication with you.
-
+<p>We at Two Rings , are highly concerned about your privacy. The information you provide us will be used solely for any future communication with you.
 * Your delivery address and phone number will be shared with the courier company.</p>
 </div>
 </div>
+<hr size= "5px" noshade color="black" />
+<h2 align="center"> Our Team </h2>
+<div class="row">
+<div class="column">
+    <figure>
+    <img src="" alt="our team" style="width:100%" />
+    <figcaption>Hiruja</figcaption>
+    </figure>
+</div>
+<div class="column">
+    <figure>
+    <img src="" alt="our team" style="width:100%" />
+    <figcaption>Malhara</figcaption>
+    </figure>
+</div>
+<div class="column">
+    <figure>
+    <img src="" alt="our team" style="width:100%" />
+    <figcaption>Kavinda</figcaption>
+    </figure>
+</div>
+<div class="column">
+    <figure>
+    <img src="" alt="our team" style="width:100%" />
+    <figcaption>Dulan</figcaption>
+    </figure>
+</div>
+<div class="column">
+    <figure>
+    <img src="" alt="our team" style="width:100%" />
+    <figcaption>Achini</figcaption>
+    </figure>
+</div>
+</div>
+
 
 
 

a5927c7c56bbc766fc1f86ee8d763313373355aa
unknown <achinimalsha4@gmail.com>
2020-10-04 15:36:40 +0530

    about us

--- aboutus.php
+++ aboutus.php
@@ -1,1 +1,20 @@
-test aboutus
+<?php
+include 'includes/header.php';
+?>
+<link rel="stylesheet" href="css/aboutus.css">
+<title>About US</title>
+</head>
+<body>
+
+<?php
+include 'includes/menu.php';
+?>
+<!-- write the code here -->
+
+
+
+
+
+<?php
+include 'includes/footer.php';
+?>

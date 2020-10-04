7f2953eb56500cabf292f05257b2e84bff19c9f3
MADU <kavimaduwantha3d@gmail.com>
2020-10-04 16:12:57 +0530

    kavinda update

---
+++ fgpw.php
@@ -0,0 +1,22 @@
+<?php
+include 'includes/header.php';
+?>
+
+<link rel="stylesheet" href="css/fgpw.css">
+<title>Signin</title>
+</head>
+<body>
+
+<!-- Code Start -->
+
+
+
+
+
+
+
+
+
+<!-- Code End -->
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,22 @@
+<?php
+include 'includes/header.php';
+?>
+
+<link rel="stylesheet" href="css/signup.css">
+<title>Signin</title>
+</head>
+<body>
+
+<!-- Code Start -->
+
+
+
+
+
+
+
+
+
+<!-- Code End -->
+</body>
+</html>

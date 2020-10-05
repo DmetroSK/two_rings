4c5a2b58af0d459421a9fcf3941eb1e187859e10
MADU <kavimaduwantha3d@gmail.com>
2020-10-05 13:59:07 +0530

    signup

--- signup.php
+++ signup.php
@@ -9,8 +9,52 @@ include 'includes/header.php';
 
 <!-- Code Start -->
 
+<div class="row">
 
-Test
+  <div class="column">
+      <img src="img/img_signin.png" alt="Login">
+  </div>
+
+  <div class="column">
+  <div class="container">
+
+    <form method="post" action="">
+        <div id="div_login">
+            <h1>Register</h1>
+            <p>Please complete to create your account</p>
+            <div>
+                <input type="text" class="textbox" id="txt_fname" name="txt_fname" placeholder="Firstname" />
+                <input type="text" class="textbox" id="txt_lname" name="txt_lname" placeholder="Lastname" />
+            </div>
+            <div>
+                <input type="text" class="textbox" id="txt_uname" name="txt_uname" placeholder="Username"/>
+            </div>
+            <div>
+                <input type="email" class="textbox" id="txt_email" name="txt_email" placeholder="Email"/>
+            </div>
+            <div>
+                <input type="password" class="textbox" id="txt_uname" name="txt_pwd" placeholder="Password"/>
+            </div>
+            <div>
+                <input type="password" class="textbox" id="txt_uname" name="txt_pwd" placeholder="Confirm Password"/>
+            </div>
+            <div>
+                <input type="checkbox" class="textbox" id="txt_agree" name="txt_agree" />I agree with the terms and conditions
+            </div>
+
+            <div id="div_btn">
+                
+                <input type="button" value="Signup" name="but_submit" id="but_submit" />
+            </div>
+        </div>
+    </form>
+</div>
+  </div>
+
+</div>
+
+
+
 
 
 

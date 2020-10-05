7f67c1bcf3556df784128ba6e1f851faafe75ca6
Dulan316 <mahamithawadulan@gmail.com>
2020-10-05 06:50:30 +0530

    index updated

--- index.php
+++ index.php
@@ -10,6 +10,8 @@ include 'includes/header.php';
 include 'includes/menu.php';
 ?>
 
+<h1 style="text-align:center">Our services</h1>
+<img src="C:\xampp 2\htdocs\two_rings\photoshome\1.jpg">
 
 <?php
 include 'includes/footer.php';

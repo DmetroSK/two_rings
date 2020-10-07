0d8234477d9a4e77856d9776424ac612825b6b96
Dulan316 <mahamithawadulan@gmail.com>
2020-10-07 08:45:02 +0530

    commit on index

--- index.php
+++ index.php
@@ -3,6 +3,23 @@ include 'includes/header.php';
 ?>
 <link rel="stylesheet" href="css/index.css">
 <title>Home</title>
+
+<style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+        .imgbox {
+            display: grid;
+            height: 100%;
+        }
+        .center-fit {
+            max-width: 100%;
+            max-height: 100%;
+            margin: auto;
+        }
+    </style>
+
 </head>
 <body>
 
@@ -10,9 +27,20 @@ include 'includes/header.php';
 include 'includes/menu.php';
 ?>
 
-<h1 style="text-align:center">Our services</h1>
-<img src="photoshome\1.jpg">
+<img class="center-fit" src="photoshome\1.jpg">
 
+<br>
+<br>
+<br>
+<br>
+<h1 style="text-align:center">Our services</h1>
+<!--<img src="photoshome\1.jpg">-->
+<br>
+
+<img class="center-fit" src="photoshome\Untitled design.png" usemap="#mapped">
+
+<map name="mapped">
+<area shape = "rect" coords="820,2111,1504,2343" href="contact.php">
 
 <?php
 include 'includes/footer.php';
